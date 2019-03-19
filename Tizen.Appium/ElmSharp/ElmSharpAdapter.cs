06fac69507e2ffc173427173e8dbaeb0d617a907
sung-su.kim <sung-su.kim@samsung.com>
2019-03-19 12:10:50 +0900

    Add ElmSharp and NUI adaptor

---
+++ Tizen.Appium/ElmSharp/ElmSharpAdapter.cs
@@ -0,0 +1,53 @@
+using System;
+using ElmSharp;
+using Tizen.Appium.ElmSharp;
+
+namespace Tizen.Appium
+{
+    public sealed class ElmSharpAdapter : BaseAdapter
+    {
+        static bool _isInitialized = false;
+
+        public static int ScreenWidth { get; private set; }
+        public static int ScreenHeight { get; private set; }
+
+        public static void Initialize()
+        {
+            if (!_isInitialized)
+            {
+                ScreenWidth = Tizen.Appium.Utils.GetScreeenWidth();
+                ScreenHeight = Tizen.Appium.Utils.GetScreenHeight();
+
+                TizenAppium.Start(new ElmSharpAdapter());
+                _isInitialized = true;
+            }
+
+        }
+
+        public static void Terminate()
+        {
+            if (_isInitialized)
+            {
+                TizenAppium.Stop();
+                _isInitialized = false;
+            }
+        }
+
+        protected override IObjectList CreateObjectList()
+        {
+            return new EvasObjectList();
+        }
+
+        protected override void AdaptApp()
+        {
+            Elementary.EvasObjectRealized += (s, e) =>
+            {
+                ObjectList.Add(s);
+            };
+            Elementary.ItemObjectRealized += (s, e) =>
+            {
+                ObjectList.Add(s);
+            };
+        }
+    }
+}
--- Tizen.Appium/NUI/NUIAdapter.cs
+++ Tizen.Appium/NUI/NUIAdapter.cs
@@ -1,17 +1,37 @@
-﻿using System;
+using System;
+using Tizen.NUI;
+using Tizen.Appium.NUI;
 
 namespace Tizen.Appium
 {
-    public class NUIAdapter : BaseAdapter
+    public class NUIAdapter : IAppAdapter
     {
-        protected override void AdaptApp()
+        NUIViewList _objectList = new NUIViewList();
+
+        IObjectList IAppAdapter.ObjectList => _objectList;
+
+        public static int ScreenWidth { get; private set; }
+        public static int ScreenHeight { get; private set; }
+
+        public NUIAdapter()
         {
-            throw new NotImplementedException();
+            Window.Instance.ViewAdded += (s, e) =>
+            {
+                _objectList.Add(s);
+            };
         }
 
-        protected override IObjectList CreateObjectList()
+        public static void Initialize()
         {
-            throw new NotImplementedException();
+            ScreenWidth = Tizen.Appium.Utils.GetScreeenWidth();
+            ScreenHeight = Tizen.Appium.Utils.GetScreenHeight();
+
+            TizenAppium.Start(new NUIAdapter());
+        }
+
+        public static void Terminate()
+        {
+            TizenAppium.Stop();
         }
     }
 }
